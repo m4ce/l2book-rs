70bf385898ea5a47d1734aa56d097ed962265159
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2023-10-10 14:39:44 +0100

    improved benchmarks

--- benches/book.rs
+++ benches/book.rs
@@ -1,21 +1,21 @@
 use std::time::Duration;
 
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use l2book_rs::ladder::{Book, DefaultBookEventListener};
-use simplerand::{rand_range, Randomable};
+use simplerand::rand_range;
 
 const DEFAULT_WORST_BID: i64 = 1000;
 const DEFAULT_BEST_BID: i64 = 2000;
 const DEFAULT_WORST_ASK: i64 = 4000;
 const DEFAULT_BEST_ASK: i64 = 3000;
+const DEFAULT_MIN_QTY: i64 = 0;
+const DEFAULT_MAX_QTY: i64 = 10;
 const TICK_SIZE: usize = 1;
 
 fn tob_benchmark(c: &mut Criterion) {
-    let listener = DefaultBookEventListener::default();
-    let mut book = Book::new(&listener);
-
-    c.bench_function("best bid", |b| {
-        book.clear();
+    c.bench_function("update best bid", |b| {
+        let listener = DefaultBookEventListener::default();
+        let mut book = Book::new(&listener);
         let mut px: i64 = DEFAULT_BEST_BID + TICK_SIZE as i64;
         b.iter(|| {
             book.begin();
@@ -25,8 +25,9 @@ fn tob_benchmark(c: &mut Criterion) {
         });
     });
 
-    c.bench_function("best ask", |b| {
-        book.clear();
+    c.bench_function("update best ask", |b| {
+        let listener = DefaultBookEventListener::default();
+        let mut book = Book::new(&listener);
         let mut px: i64 = DEFAULT_BEST_ASK - TICK_SIZE as i64;
         b.iter(|| {
             book.begin();
@@ -37,9 +38,64 @@ fn tob_benchmark(c: &mut Criterion) {
     });
 }
 
+fn random_benchmark(c: &mut Criterion) {
+    let levels = [1024, 8192, 65536];
+    for size in levels {
+        c.bench_with_input(
+            BenchmarkId::new("random update bid", size),
+            &size,
+            |b, &size| {
+                let listener = DefaultBookEventListener::default();
+                let mut book = Book::new(&listener);
+                let bids: Vec<(i64, i64)> = (0..size)
+                    .map(|_| {
+                        (
+                            rand_range::<i64>(DEFAULT_WORST_BID, DEFAULT_BEST_BID),
+                            rand_range::<i64>(DEFAULT_MIN_QTY, DEFAULT_MAX_QTY),
+                        )
+                    })
+                    .collect();
+                let mut i = 0;
+                b.iter(|| {
+                    book.begin();
+                    let index = i & (size - 1);
+                    book.update_bid(bids[index].0, bids[index].1, 1);
+                    i += 1;
+                    book.end();
+                });
+            },
+        );
+
+        c.bench_with_input(
+            BenchmarkId::new("random update ask", size),
+            &size,
+            |b, &size| {
+                let listener = DefaultBookEventListener::default();
+                let mut book = Book::new(&listener);
+                let asks: Vec<(i64, i64)> = (0..size)
+                    .map(|_| {
+                        (
+                            rand_range::<i64>(DEFAULT_BEST_ASK, DEFAULT_WORST_ASK),
+                            rand_range::<i64>(DEFAULT_MIN_QTY, DEFAULT_MAX_QTY),
+                        )
+                    })
+                    .collect();
+                let mut i = 0;
+                b.iter(|| {
+                    book.begin();
+                    let index = i & (size - 1);
+                    book.update_ask(asks[index].0, asks[index].1, 1);
+                    i += 1;
+                    book.end();
+                });
+            },
+        );
+    }
+}
+
 criterion_group! {
     name = benches;
     config = Criterion::default().measurement_time(Duration::from_secs(10));
-    targets = tob_benchmark
+    targets = tob_benchmark, random_benchmark
 }
 criterion_main!(benches);
